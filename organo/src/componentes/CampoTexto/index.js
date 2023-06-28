87495ac1028bfa0bf035138a6767ed18a03c5986
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2023-06-28 18:54:03 +0000

    pagina inicial finalizada

---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,15 @@
+import './Campotexto.css'
+
+const CampoTexto = (props) => {
+
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>            
+        </div>  
+    )
+
+
+}
+
+export default CampoTexto
